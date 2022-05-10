3714cf07e71fdb7a988e33c858743548d43e6003
Eran Cohen <er11cohen@gmail.com>
2022-05-10 12:59:27 +0300

    Bug fix for parse CSV file

--- tahara/src/main/java/com/eran/tahara/UtilTahara.java
+++ tahara/src/main/java/com/eran/tahara/UtilTahara.java
@@ -12,7 +12,7 @@ public class UtilTahara {
         String text = Utils.ReadTxtFile("files/tahara.csv", context);
         ArrayList<Halach> alHalach = new ArrayList<Halach>();
         try {
-            String[] items = text.split("\\r");
+            String[] items = text.split("\\n");
             for (int i = 0; i < items.length; i++) {
                 String[] halachStr = items[i].split(",");
                 Halach halach = new Halach(Integer.parseInt(halachStr[0].trim()), halachStr[1].trim(),
